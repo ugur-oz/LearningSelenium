e3b1c6a6952d9ed1bd841d92d931d04cce8b2a16
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-03-31 21:28:30 +0200

    edit

--- WorkingWithChrome.cs
+++ WorkingWithChrome.cs
@@ -4,42 +4,42 @@ using OpenQA.Selenium.Chrome;
 
 namespace LearningSelenium
 {
-public class NewBaseType
-{
-    IWebDriver Driver;
-
-    [SetUp]
-    public void Setup()
+    public class NewBaseType
     {
-        Driver = new ChromeDriver();
+        IWebDriver Driver;
 
-        Driver.Manage().Cookies.DeleteAllCookies();
-        Driver.Manage().Window.Maximize();
-        Driver.Manage().Timeouts().PageLoad = System.TimeSpan.FromSeconds(60);
-        Driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(10);
-        Driver.Url = "https://test.qatechhub.com";
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+
+            Driver.Manage().Cookies.DeleteAllCookies();
+            Driver.Manage().Window.Maximize();
+            Driver.Manage().Timeouts().PageLoad = System.TimeSpan.FromSeconds(60);
+            Driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(10);
+            Driver.Url = "https://test.qatechhub.com";
+
+        }
+        [Test]
+        public void VerifyTitleOfThePage()
+        {
+
+            string expectedTitle = "Test QA Tech Hub – Learning By Doing is the best way to learn!";
+
+            string actualTitle = Driver.Title;
+
+            Assert.AreEqual(expectedTitle, actualTitle);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+
+        {
+            Driver.Quit();
+        }
 
     }
-    [Test]
-    public void VerifyTitleOfThePage() {
-
-        string expectedTitle = "Test QA Tech Hub – Learning By Doing is the best way to learn!";
-
-        string actualTitle = Driver.Title;
-
-        Assert.AreEqual(expectedTitle,actualTitle);
-
-     }
-
-    [TearDown]
-    public void TearDown()
-
-    {
-        Driver.Quit();
-    }
-
-  
-}
 
 
 }
